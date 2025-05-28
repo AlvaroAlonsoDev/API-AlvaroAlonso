d73092f97456015f90ac6ac140f2f2b98e83d3af
AlvaroAlonsoDev <airuritac@gmail.com>
2025-05-29 00:25:18 +0200

    fix: clean code and constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,3 +8,12 @@ export const restrictedFields = [
     "emailVerificationToken",
     "trustScore"
 ];
+
+export const testUser = {
+    email: "followtestuser@example.com",
+    password: "test1234",
+    handle: "followtesthandle",
+    displayName: "Follow Test User"
+};
+
+export const targetUserId = "68372fb26e490b47e8777793";
