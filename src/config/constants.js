0029ee62e5f71ebc8216f63f0c47d716d7f835c1
AlvaroAlonsoDev <airuritac@gmail.com>
2025-05-30 16:31:07 +0200

    update constants file

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,5 @@
+export const expiresToken = "100h";
+
 // Campos que se deben excluir al devolver datos del usuario al frontend
 export const restrictedFields = [
     "passwordHash",
@@ -9,11 +11,20 @@ export const restrictedFields = [
     "trustScore"
 ];
 
+
 export const testUser = {
-    email: `test@test.com`,
+    email: `test${Date.now()}@test.com`,
     password: "test1234",
-    handle: `testhandle`,
+    handle: `testhandle${Date.now()}`,
     displayName: "Follow Test User"
 };
 
 export const targetUserId = "68372fb26e490b47e8777793";
+
+export const VALID_RATING_ASPECTS = [
+    "sincerity",
+    "kindness",
+    "trust",
+    "vibe",
+    "responsibility"
+];
