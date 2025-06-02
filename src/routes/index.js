0941a3c05e6f6b8e1a2f8486416a6ac7c87582e1
AlvaroAlonsoDev <airuritac@gmail.com>
2025-06-02 10:54:10 +0200

    Update index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import authRouter from './auth.js';
 import followRouter from './follow.js';
 import ratingRouter from './rating.js';
+import postRouter from './post.js';
+import postLikeRouter from './postLike.js';
 
 const router = Router();
 
 router.use('/api/auth', authRouter);
 router.use('/api/follow', followRouter);
-router.use('/api/rating', ratingRouter)
+router.use('/api/rating', ratingRouter);
+router.use('/api/post', postRouter);
+router.unsubscribe('/api/like', postLikeRouter);
 
 export { router };
